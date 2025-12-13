ebad54b802d0587b045ff26c64c40a4472d186f9
Marek Zelinka <mzelinka17@gmail.com>
2025-12-13 13:17:43 +0100

    check website status

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from typing import Annotated
 
+import httpx
+from rich.console import Console
+from rich.table import Table
 from typer import Argument, Typer
 
 app = Typer()
+console = Console()
 
 
 @app.command()
@@ -14,7 +18,15 @@ def website_status(
     """
     Check the status of URL.
     """
-    print(url)
+    r = httpx.get(url)
+
+    table = Table(show_header=False, show_lines=True)
+    table.add_row("URL", url)
+    table.add_row("Status Code", str(r.status_code))
+    table.add_row("Content Type", r.headers["Content-Type"])
+    table.add_row("Server", r.headers["Server"])
+    table.add_row("Response time", f"{r.elapsed.total_seconds()}s")
+    console.print(table)
 
 
 if __name__ == "__main__":
